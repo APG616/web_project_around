a72ebd27216738015797877b14bbf1af0478cbef
DanielPatino <andpague@protonmail.com>
2024-09-23 11:20:00 -0500

    Second Commit: Actulización js y correcciones CSS

--- scripts/index.js
+++ scripts/index.js
@@ -1,28 +1,40 @@
-const popupContainer = document.querySelector("#form-profile");
-const profileButton = document.querySelector(".profile__edit-button");
-const profileName = document.querySelector(".profile__info-name");
-const profileDescription = document.querySelector(".profile__info-description");
-const inputName = document.querySelector("#name");
-const inputDescription = document.querySelector("#about-me");
-const formProfile = document.querySelector("#form-profile");
-const nameInput = document.querySelector(".form__name");
-const closeButton = document.querySelector(".popup__close");
+const profilePopupButton = document.querySelector(".profile__edit-button");
+const displayWindow = document.querySelectorAll(".popup");
+const profilebuttonClose = document.querySelectorAll(".popup__close");
+const profilePopup = document.querySelector("#form-profile");
+const nameInput = document.querySelector(".popup__input");
+const jobInput = document.querySelector(".form__about-me");
+const profileButton = document.querySelector("#edit-submit");
 
-function handleOpenProfile(evt) {
-  popupContainer.classList.add(".popup__open");
-}
-
-function handleCloseProfile(evt) {
-  popupContainer.classList.remove(".popup__open");
-}
-
-profileButton.addEventListener("click", handleOpenProfile);
-
-closeButton.addEventListener("click", handleCloseProfile);
-
-formProfile.addEventListener("submit", function (evt) {
-  evt.preventDefault();
-  profileName.textContent = inputName.value;
-  profileHobbie.textContent = inputHobbie.value;
-  handleCloseProfile();
+profilePopupButton.addEventListener("click", function () {
+  profilePopup.classList.add("popup_open");
 });
+
+function handleClosePopup(evt) {
+  profilePopup.classList.remove("popup_open");
+  resetValidation({
+    formSelector: ".popup__form",
+    inputSelector: ".popup__input",
+    submitButtonSelector: ".form__send",
+    inactiveButtonClass: "popup__button_disabled",
+    inputErrorClass: "popup__input_type_error",
+    errorClass: "popup__error_visible",
+  });
+}
+profilebuttonClose[0].addEventListener("click", handleClosePopup);
+
+profilePopupAdd.addEventListener("click", function () {
+  cardPopup.classList.add("popup_opened");
+});
+
+function handleClosePopupAdd(evt) {
+  cardPopup.classList.remove("popup_opened");
+  resetValidation({
+    formSelector: ".popup__form",
+    inputSelector: ".popup__input",
+    submitButtonSelector: ".form__send",
+    inactiveButtonClass: "popup__button_disabled",
+    inputErrorClass: "popup__input_type_error",
+    errorClass: "popup__error_visible",
+  });
+}
